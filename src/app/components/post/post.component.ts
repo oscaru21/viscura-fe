e3625b25d2cd87c74195cbd2fb337dff6f0eb84d
Oscar Umana <oscarumana1021@gmail.com>
2024-11-11 21:04:45 -0500

    add images

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -17,5 +17,6 @@ import { PostFormComponent } from '../post-form/post-form.component';
   styleUrl: './post.component.scss'
 })
 export class PostComponent {
+  images = ['http://localhost:8000/events/1/photos/29.png', 'http://localhost:8000/events/1/photos/15.png', 'http://localhost:8000/events/1/photos/13.png', 'http://localhost:8000/events/1/photos/11.png'];
   items = Array.from({ length: 5}, (_, i) => i + 1);
 }
